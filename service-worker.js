a4d6b0aad978e847dace3226676146fda3804974
greathenrymao <greathenrymao@gmail.com>
2026-09-04 15:11:29 +0800

    Refresh interface for iOS 27

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="id-interview-english-v1";
+const CACHE="id-interview-english-v2-ios27";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./data/lessons.json","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
